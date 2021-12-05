b7bb747fd3336e6546e27e5e692567f38d851bbc
YUYING-toro <55909729+YUYING-toro@users.noreply.github.com>
2021-12-05 16:45:22 -0500

    Update Logout.php

--- Logout.php
+++ Logout.php
@@ -1,13 +1,8 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<?php 
     session_start();
-    session_destroy();
-    header("Location:home.php");
+    //load header
 
+    session_destroy();
+        header("Location: index.php");
+        exit();
 ?>

34f171e7e6852826584f34ecc7441c86a03a605c
YUYING-toro <55909729+YUYING-toro@users.noreply.github.com>
2021-12-05 16:49:51 -0500

    Update MyAlbums.php

--- MyAlbums.php
+++ MyAlbums.php
@@ -1,11 +1,36 @@
 <?php 
-    include('./common/Header.php'); ?>
+    session_start();
+    if(!isset($_SESSION['user']) || $_SESSION['user'] === '') {
+         header("Location: Index.php");
+        
+    } else {
+        $userId =$_SESSION['user'];
+    } 
+
+    include('./common/Header.php'); 
+    ?>
+<?php 
+$dbConnection = parse_ini_file("Project.ini");
+extract($dbConnection);
+$conn = new PDO($dsn, $user, $password);
+//GET NAME
+$sqlName = "SELECT name FROM user where userId='{$userId}'";
+$queryCourse = $conn->prepare($sqlName);
+$queryCourse->execute();
+$courseInfo = $queryCourse->fetch(PDO::FETCH_ASSOC);
+$userName = $courseInfo["name"];
+$_SESSION['userName']= $userName;
+// Load Access Authority options
+
+    
+?>
+
 
 <div class="container">
-  <h3> My album page</h3
-  </div>
-<?php 
-    include('./common/Footer.php'); ?>
-
-
-
+    <h3> My Albums</h3>
+    <h4>Welcome  <?php print_r($userName); ?> (not you? change user <a href="Login.php">here</a>)</h4>
+    <p><a href="AddAlbum.php">Create a New Album</a></p>
+    
+  
+</div>
+<?php include('./common/Footer.php'); ?>
